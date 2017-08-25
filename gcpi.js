3e6b3d66bd888d13e0b936fc3db1ccbfbdfb8b59
rosivanov <ros.g6w@gmail.com>
2017-08-25 17:43:52 +0300

    add copy btn

--- gcpi.js
+++ gcpi.js
@@ -1,5 +1,6 @@
 //=include ./node_modules/ua-parser-js/src/ua-parser.js
 import UAParser from 'ua-parser-js';
+import Clipboard from 'clipboard';
 
 'use strict';
 (function() {
@@ -22,16 +23,35 @@ import UAParser from 'ua-parser-js';
 		add_info: function () {
 			var info_div,
 			html = '',
+			clipboard = '',
 			uaparser = new UAParser(),
 			uaobject = uaparser.getResult();
 
 			for (var category in uaobject) {
 				if ( uaobject[category] === 'ua' || typeof uaobject[category] !== 'object' ) continue;
 				html += '<div>'+ category +'</div>';
+				clipboard += category + '\r\n';
 				for (var category_item in uaobject[category]) {
 					html += '<div>'+ category_item + ': '+ uaobject[category][category_item] +'</div>';
+					clipboard += category_item + ': '+ uaobject[category][category_item] + '\r\n';
 				}
 			}
+
+			clipboard += 'screen' + '\r\n';
+			clipboard += 'screen width: '+ window.screen.width + '\r\n';
+			clipboard += 'screen availWidth: '+ window.screen.availWidth + '\r\n';
+			clipboard += 'screen height: '+ window.screen.height + '\r\n';
+			clipboard += 'screen availHeight: '+ window.screen.availHeight + '\r\n';
+			clipboard += 'viewport' + '\r\n';
+			clipboard += 'viewport width: '+ window.innerWidth + '\r\n';
+			clipboard += 'viewport height: '+ window.innerHeight + '\r\n';
+			clipboard += 'document' + '\r\n';
+			clipboard += 'documentEl clientWidth: '+ document.documentElement.clientWidth + '\r\n';
+			clipboard += 'documentEl offsetWidth: '+ document.documentElement.offsetWidth + '\r\n';
+			clipboard += 'documentEl scrollWidth: '+ document.documentElement.scrollWidth + '\r\n';
+			clipboard += 'documentEl clientHeight: '+ document.documentElement.clientHeight + '\r\n';
+			clipboard += 'documentEl offsetHeight: '+ document.documentElement.offsetHeight + '\r\n';
+			clipboard += 'documentEl scrollHeight: '+ document.documentElement.scrollHeight + '\r\n';
 
 			html += '<div>screen</div>';
 			html += '<div>screen width: '+ window.screen.width +'</div>';
@@ -48,11 +68,18 @@ import UAParser from 'ua-parser-js';
 			html += '<div>documentEl clientHeight: '+ document.documentElement.clientHeight +'</div>';
 			html += '<div>documentEl offsetHeight: '+ document.documentElement.offsetHeight +'</div>';
 			html += '<div>documentEl scrollHeight: '+ document.documentElement.scrollHeight +'</div>';
+			html += '<div>documentEl scrollHeight: '+ document.documentElement.scrollHeight +'</div>';
 
+			html += '<button data-clipboard-text=\"' + clipboard + '\" type="button" >Copy</button>';
+
+			// create element
 			info_div = document.createElement('div');
 			info_div.style.cssText = GCPI.styles;
 			info_div.innerHTML = html;
 			document.body.appendChild(info_div);
+
+			new Clipboard('button[data-clipboard-text]'); // init Clipboard
+
 			return;
 		}
 	};
